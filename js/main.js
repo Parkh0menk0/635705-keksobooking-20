07af3874af8c50ce209d4a10e291ce4cabf56bc6
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-06-12 17:52:48 +0300

    Поправлет ошибки валидатора

--- js/main.js
+++ js/main.js
@@ -1,47 +1,47 @@
-"use strict";
+'use strict';
 
 var ADS_COUNT = 8;
 
 var TITLE = [
-  "Большая уютная квартира",
-  "Маленькая неуютная квартира",
-  "Огромный прекрасный дворец",
-  "Маленький ужасный дворец",
-  "Красивый гостевой домик",
-  "Некрасивый негостеприимный домик",
-  "Уютное бунгало далеко от моря",
-  "Неуютное бунгало по колено в воде"
-]
+  'Большая уютная квартира',
+  'Маленькая неуютная квартира',
+  'Огромный прекрасный дворец',
+  'Маленький ужасный дворец',
+  'Красивый гостевой домик',
+  'Некрасивый негостеприимный домик',
+  'Уютное бунгало далеко от моря',
+  'Неуютное бунгало по колено в воде'
+];
 
 var TYPE = [
-  "palace",
-  "flat",
-  "house",
-  "bungalo"
+  'palace',
+  'flat',
+  'house',
+  'bungalo'
 ];
 
 var TIME = [
-  "12:00",
-  "13:00",
-  "14:00"
+  '12:00',
+  '13:00',
+  '14:00'
 ];
 
 var FEATURES = [
-  "wifi",
-  "dishwasher",
-  "parking",
-  "washer",
-  "elevator",
-  "conditioner"
-]
+  'wifi',
+  'dishwasher',
+  'parking',
+  'washer',
+  'elevator',
+  'conditioner'
+];
 
 var PHOTOS = [
-  "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-  "http://o0.github.io/assets/images/tokyo/hotel2.jpg",
-  "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-]
+  'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg'
+];
 
-var map = document.querySelector(".map");
+var map = document.querySelector('.map');
 
 /**
  * @description Генерирует случайные данные.
@@ -65,32 +65,34 @@ function randomInteger(min, max) {
  */
 function createAds(count, title, type, time, features, photos) {
   var ads = [];
+
   for (var i = 0; i < count; i++) {
-    ads.push(
-      {
-        "author": {
-          "avatar": "img/avatars/user0" + (++i) + ".png"
-        },
-        "offer": {
-          "title": title[i],
-          "address": location.x + ", " + location.y,
-          "price": randomInteger(1000, 1000000),
-          "type": type[randomInteger(0, type.length - 1)],
-          "rooms": randomInteger(1, 5),
-          "guests": randomInteger(1, Number.MAX_SAFE_INTEGER),
-          "checkin": time[randomInteger(0, time.length - 1)],
-          "checkout": time[randomInteger(0, time.length - 1)],
-          "features": features,
-          "description": "",
-          "photos": photos
-        },
-        "location": {
-          "x": randomInteger(130, 630),
-          "y": randomInteger(130, 630)
-        }
+    var ad = {
+      'author': {
+        'avatar': 'img/avatars/user0' + (i + 1) + '.png'
+      },
+      'offer': {
+        'title': title[i],
+        'address': location.x + ', ' + location.y,
+        'price': randomInteger(1000, 1000000),
+        'type': type[randomInteger(0, type.length - 1)],
+        'rooms': randomInteger(1, 5),
+        'guests': randomInteger(1, Number.MAX_SAFE_INTEGER),
+        'checkin': time[randomInteger(0, time.length - 1)],
+        'checkout': time[randomInteger(0, time.length - 1)],
+        'features': features,
+        'description': '',
+        'photos': photos
+      },
+      'location': {
+        'x': randomInteger(0, 1150),
+        'y': randomInteger(130, 630)
       }
-    );
+    };
+
+    ads.push(ad);
   }
+
   return ads;
 }
 
@@ -100,14 +102,14 @@ function createAds(count, title, type, time, features, photos) {
  * @return {Object} DocumentFragment.
  */
 function createMarks(generated) {
-  var template = document.querySelector("#pin").content.querySelector("button");
+  var template = document.querySelector('#pin').content.querySelector('button');
   var fragment = document.createDocumentFragment();
 
   for (var i = 0; i < generated.length; i++) {
     var element = template.cloneNode(true);
-    var avatar = element.querySelector(".map__pin img");
-    element.style.left = generated[i].location.x + "px";
-    element.style.top = generated[i].location.y + "px";
+    var avatar = element.querySelector('.map__pin img');
+    element.style.left = generated[i].location.x - element.offsetWidth / 2 + 'px';
+    element.style.top = generated[i].location.y - element.offsetHeight / 2 + 'px';
     avatar.src = generated[i].author.avatar;
     avatar.alt = generated[i].offer.title;
     fragment.appendChild(element);
@@ -126,6 +128,6 @@ function fillMarks(fragment) {
   list.appendChild(fragment);
 }
 
-map.classList.remove("map--faded");
+map.classList.remove('map--faded');
 
 fillMarks(createMarks(createAds(ADS_COUNT, TITLE, TYPE, TIME, FEATURES, PHOTOS)));
