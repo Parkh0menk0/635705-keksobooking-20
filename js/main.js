2339016df60303c25c2b223a3792c32163869518
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-06-14 21:34:03 +0300

    Поправляет функцию создания карточки

--- js/main.js
+++ js/main.js
@@ -139,49 +139,50 @@ function createMarks(generated) {
 }
 
 /**
- * @description Создаёт DOM-элементы карточек объявления на основе JS-объекта.
+ * @description Создаёт DOM-элемент карточеки объявления на основе первого элемента массива JS-объектов.
  * @param {Object[]} generated Сгенерированный массив объявлений.
  * @return {Object} DocumentFragment.
  */
-function createCards(generated) {
+function createCard(generated) {
   var template = document.querySelector('#card').content.querySelector('article');
   var fragment = document.createDocumentFragment();
   var element = template.cloneNode(true);
+  var first = generated[0];
   var avatar = element.querySelector('.popup__avatar');
   var photos = element.querySelector('.popup__photos');
 
-  element.querySelector('.popup__title').textContent = generated[0].offer.title;
-  element.querySelector('.popup__text--address').textContent = generated[0].offer.address;
-  element.querySelector('.popup__text--price').textContent = generated[0].offer.price + '₽/ночь';
-  element.querySelector('.popup__type').textContent = generated[0].offer.type;
-  element.querySelector('.popup__text--capacity').textContent = generated[0].offer.rooms + ' комнаты для ' + generated[0].offer.guests + ' гостей';
-  element.querySelector('.popup__text--time').textContent = 'Заезд после ' + generated[0].offer.checkin + ', выезд до ' + generated[0].offer.checkout;
+  element.querySelector('.popup__title').textContent = first.offer.title;
+  element.querySelector('.popup__text--address').textContent = first.offer.address;
+  element.querySelector('.popup__text--price').textContent = first.offer.price + '₽/ночь';
+  element.querySelector('.popup__type').textContent = first.offer.type;
+  element.querySelector('.popup__text--capacity').textContent = first.offer.rooms + ' комнаты для ' + first.offer.guests + ' гостей';
+  element.querySelector('.popup__text--time').textContent = 'Заезд после ' + first.offer.checkin + ', выезд до ' + first.offer.checkout;
 
-  if (generated[0].offer.features.includes('wifi')) {
-    element.querySelector('.popup__feature--wifi').textContent = generated[0].offer.features[0];
-  } else if (generated[0].offer.features.includes('dishwasher')) {
-    element.querySelector('.popup__feature--dishwasher').textContent = generated[0].offer.features[1];
-  } else if (generated[0].offer.features.includes('parking')) {
-    element.querySelector('.popup__feature--parking').textContent = generated[0].offer.features[2];
-  } else if (generated[0].offer.features.includes('washer')) {
-    element.querySelector('.popup__feature--washer').textContent = generated[0].offer.features[3];
-  } else if (generated[0].offer.features.includes('elevator')) {
-    element.querySelector('.popup__feature--elevator').textContent = generated[0].offer.features[4];
-  } else if (generated[0].offer.features.includes('conditioner')) {
-    element.querySelector('.popup__feature--conditioner').textContent = generated[0].offer.features[5];
+  if (first.offer.features.includes('wifi')) {
+    element.querySelector('.popup__feature--wifi').textContent = first.offer.features[0];
+  } else if (first.offer.features.includes('dishwasher')) {
+    element.querySelector('.popup__feature--dishwasher').textContent = first.offer.features[1];
+  } else if (first.offer.features.includes('parking')) {
+    element.querySelector('.popup__feature--parking').textContent = first.offer.features[2];
+  } else if (first.offer.features.includes('washer')) {
+    element.querySelector('.popup__feature--washer').textContent = first.offer.features[3];
+  } else if (first.offer.features.includes('elevator')) {
+    element.querySelector('.popup__feature--elevator').textContent = first.offer.features[4];
+  } else if (first.offer.features.includes('conditioner')) {
+    element.querySelector('.popup__feature--conditioner').textContent = first.offer.features[5];
   }
 
-  element.querySelector('.popup__title').textContent = generated[0].offer.title;
+  element.querySelector('.popup__title').textContent = first.offer.title;
 
   for (var i = 0; i < photos.children.length; i++) {
     if (i > 1) {
       photos.append(document.createElement('img'));
     }
     var photo = photos.children[i];
-    photo.src = generated[0].offer.photos[i];
+    photo.src = first.offer.photos[i];
   }
 
-  avatar.src = generated[0].author.avatar;
+  avatar.src = first.author.avatar;
 
   fragment.appendChild(element);
 
@@ -202,4 +203,4 @@ map.classList.remove('map--faded');
 
 fillMarks(createMarks(createAds(ADS_COUNT, TITLE, TYPE, TIME, FEATURES, PHOTOS)));
 
-map.insertBefore(createCards(createAds(ADS_COUNT, TITLE, TYPE, TIME, FEATURES, PHOTOS)), filtersContainer);
+map.insertBefore(createCard(createAds(ADS_COUNT, TITLE, TYPE, TIME, FEATURES, PHOTOS)), filtersContainer);
