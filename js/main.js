c7943e2fb28c1e814a85fbbde3dfae63212863a5
Oleg Parkhomenko <Oleg.Parkhom@gmail.com>
2020-06-14 20:10:44 +0300

    Создаёт DOM-элемент объявления (карточка объявления) на основе шаблона #card, заполненного данными из первого по порядку элемента из сгенерированного массива объектов

--- js/main.js
+++ js/main.js
@@ -47,6 +47,7 @@ var OFFSET = {
 };
 
 var map = document.querySelector('.map');
+var filtersContainer = map.querySelector('.map__filters-container');
 
 /**
  * @description Генерирует случайные данные.
@@ -115,7 +116,7 @@ function createAds(count, title, type, time, features, photos) {
 }
 
 /**
- * @description Создаёт DOM-элементы на основе JS-объекта.
+ * @description Создаёт DOM-элементы отметок на карте на основе JS-объекта.
  * @param {Object[]} generated Сгенерированный массив объявлений.
  * @return {Object} DocumentFragment.
  */
@@ -132,6 +133,56 @@ function createMarks(generated) {
     avatar.alt = generated[i].offer.title;
     fragment.appendChild(element);
   }
+
+  return fragment;
+}
+
+/**
+ * @description Создаёт DOM-элементы карточек объявления на основе JS-объекта.
+ * @param {Object[]} generated Сгенерированный массив объявлений.
+ * @return {Object} DocumentFragment.
+ */
+function createCards(generated) {
+  var template = document.querySelector('#card').content.querySelector('article');
+  var fragment = document.createDocumentFragment();
+  var element = template.cloneNode(true);
+  var avatar = element.querySelector('.popup__avatar');
+  var photos = element.querySelector('.popup__photos');
+
+  element.querySelector('.popup__title').textContent = generated[0].offer.title;
+  element.querySelector('.popup__text--address').textContent = generated[0].offer.address;
+  element.querySelector('.popup__text--price').textContent = generated[0].offer.price + '₽/ночь';
+  element.querySelector('.popup__type').textContent = generated[0].offer.type;
+  element.querySelector('.popup__text--capacity').textContent = generated[0].offer.rooms + ' комнаты для ' + generated[0].offer.guests + ' гостей';
+  element.querySelector('.popup__text--time').textContent = 'Заезд после ' + generated[0].offer.checkin + ', выезд до ' + generated[0].offer.checkout;
+
+  if (generated[0].offer.features.includes('wifi')) {
+    element.querySelector('.popup__feature--wifi').textContent = generated[0].offer.features[0];
+  } else if (generated[0].offer.features.includes('dishwasher')) {
+    element.querySelector('.popup__feature--dishwasher').textContent = generated[0].offer.features[1];
+  } else if (generated[0].offer.features.includes('parking')) {
+    element.querySelector('.popup__feature--parking').textContent = generated[0].offer.features[2];
+  } else if (generated[0].offer.features.includes('washer')) {
+    element.querySelector('.popup__feature--washer').textContent = generated[0].offer.features[3];
+  } else if (generated[0].offer.features.includes('elevator')) {
+    element.querySelector('.popup__feature--elevator').textContent = generated[0].offer.features[4];
+  } else if (generated[0].offer.features.includes('conditioner')) {
+    element.querySelector('.popup__feature--conditioner').textContent = generated[0].offer.features[5];
+  }
+
+  element.querySelector('.popup__title').textContent = generated[0].offer.title;
+
+  for (var i = 0; i < photos.children.length; i++) {
+    if (i > 1) {
+      photos.append(document.createElement('img'));
+    }
+    var photo = photos.children[i];
+    photo.src = generated[0].offer.photos[i];
+  }
+
+  avatar.src = generated[0].author.avatar;
+
+  fragment.appendChild(element);
 
   return fragment;
 }
